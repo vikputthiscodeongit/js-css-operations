d7b881d6500abddeb51111df772fa8aed378966b
vikputthiscodeongit <code@viktorchin.nl>
2022-03-24 17:09:44 +0100

    Fully specify imports
    Fixes errors when modules are bundled with Webpack.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { default as getPropValue } from "./getPropValue";
-export { default as getUnit } from "./getUnit";
-export { default as motionAllowed } from "./mediaMotion";
-export { default as timeToMs } from "./timeToMs";
+export { default as getPropValue } from "./getPropValue.js";
+export { default as getUnit } from "./getUnit.js";
+export { default as motionAllowed } from "./mediaMotion.js";
+export { default as timeToMs } from "./timeToMs.js";
